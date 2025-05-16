ca135dc07bd9da44cd1de1d647af8735cd8c04d0
sunstones03 <sun.stones2003@gmail.com>
2025-05-16 13:28:28 -0400

    added flask backend with deepseek agent

---
+++ case-study/backend/app.py
@@ -0,0 +1,81 @@
+from flask import Flask, jsonify, request, session
+from flask_cors import CORS
+
+from langchain_anthropic import ChatAnthropic
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import HumanMessage, SystemMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import create_react_agent
+from langchain_deepseek import ChatDeepSeek
+from langchain.prompts import (
+    SystemMessagePromptTemplate,
+    PromptTemplate,
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate
+)
+
+import numpy as np
+
+system_prompt = """
+You are an expert support agent for partselect.com.
+Your role is to answer user questions related to refrigerator and dishwasher parts strictly and exclusively using the information found on the company website partselect.com.
+
+Respond only and exclusively using the information contained in the provided website.
+Do not introduce any information that is not present in the website.
+If the provided website does not contain sufficient information to answer the question, or if the website does not directly address the user’s query, redirect the user to customer support then prompt them to ask a question within your expertise in a happy manner.
+
+Do not explain your answer or provide any additional commentary.
+Your responses should be concise and focused on addressing the user's query using only the provided information.
+
+Adhere to the context and limitations at all times.
+If any part of the question cannot be answered with the provided website, you must refrain from speculation or the use of external knowledge.
+
+Ask follow up questions if necessary.
+Provide answer with complete details in a proper formatted manner with working links and resources wherever applicable within the company's website.
+Never provide wrong links.
+"""
+
+app = Flask(__name__)
+CORS(app)  # Allow CORS for all routes
+app.config["SECRET_KEY"] = "secret"
+
+@app.route('/api/chat', methods=["POST","GET"])
+def chat():
+    if request.method == "POST":
+
+        data = request.get_json()
+        query = data.get('query', '')
+        new_thread = data.get('new_thread', '')
+
+        ## check if starting a new thread, 
+        ## if so then reset thread and send system prompt
+        if new_thread:
+            session['thread_id'] = np.random.randint(10000)
+            config = {"configurable" : {"thread_id" : session['thread_id']}}
+            msg = {"messages": [SystemMessage(content=system_prompt)]}
+            agent_executor.invoke(msg, config)
+
+        ## create input for llm agent
+        config = {"configurable" : {"thread_id" : session['thread_id']}}
+        msg = {"messages": [HumanMessage(content=query)]}
+
+        answer = agent_executor.invoke(msg, config)["messages"][-1].content
+        print(session['thread_id'])
+        return jsonify({"answer": answer})
+
+if __name__ == '__main__':
+
+    memory = MemorySaver()
+    model = ChatDeepSeek(
+        model="deepseek-chat",
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+    )
+    search = TavilySearchResults(max_results=2)
+    tools = [search]
+    agent_executor = create_react_agent(model, tools, checkpointer=memory)
+
+    ## set up flask server
+    app.run(debug=True)
--- case-study/src/api/api.js
+++ case-study/src/api/api.js
@@ -1,11 +1,27 @@
-
 export const getAIMessage = async (userQuery) => {
+  try {
+    const response = await fetch('http://localhost:5000/api/chat', 
+      { method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ query : userQuery, new_thread : true })
+      });
 
-  const message = 
-    {
+    const data = await response.json()
+
+    const message = {
       role: "assistant",
-      content: "Connect your backend here...."
+      content: data.answer
     }
 
-  return message;
+    return message
+  }
+  catch (error) {
+    console.error("failed to call Flask server");
+    const message = {
+      role: "assistant",
+      content: "error in calling Flask server"
+    };
+
+    return message
+  }
 };
